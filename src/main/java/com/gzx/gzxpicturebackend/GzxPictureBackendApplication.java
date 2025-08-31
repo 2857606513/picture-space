ac3df1e0adf087a4bae482b86e9137b2ff01daab
guozhongxing <2857606513@qq.com>
2025-08-31 18:08:11 +0800

    第一次提交

---
+++ src/main/java/com/gzx/gzxpicturebackend/GzxPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gzx.gzxpicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GzxPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GzxPictureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gzx/gzxpicturebackend/GzxPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gzx.gzxpicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GzxPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
